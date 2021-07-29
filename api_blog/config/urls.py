d72cc2ca65f430c495c1cee002d6a274db30b03c
Igor <iksendikov@gmail.com>
2021-07-29 17:05:44 +0300

    developed a backend for a blog using drf

---
+++ api_blog/config/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('api/token/',
+         TokenObtainPairView.as_view(),
+         name='token'),
+    path('api/refresh_token/',
+         TokenRefreshView.as_view(),
+         name='refresh_token'),
+    path('ckeditor/', include('ckeditor_uploader.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ api_blog/core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (AsideView, CommentView, FeedBackView, PostViewSet,
+                    ProfileView, RegisterView, TagDetailView, TagView)
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('tags/', TagView.as_view()),
+    path('tags/<slug:tag_slug>/', TagDetailView.as_view()),
+    path('aside/', AsideView.as_view()),
+    path('feedback/', FeedBackView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('profile/', ProfileView.as_view()),
+    path('comments/', CommentView.as_view()),
+    path('comments/<slug:post_slug>/', CommentView.as_view()),
+]
